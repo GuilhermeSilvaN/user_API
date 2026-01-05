c33837980a6db501b3e0de650d246aa9e597b66e
GuilhermeSilvaN <guilhermesilvaalvescardoso@gmail.com>
2026-01-05 20:07:22 -0300

    first commit

---
+++ src/main/java/com/userCompany/usercad/UsercadApplication.java
@@ -0,0 +1,13 @@
+package com.userCompany.usercad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsercadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsercadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/userCompany/usercad/UsercadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userCompany.usercad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsercadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
